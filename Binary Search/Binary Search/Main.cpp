f8475804ae057d740a1e313f7427f2d9da2bc3e9
Parsa KamaliPour <parsakamalipour@gmail.com>
2020-02-08 22:57:24 +0330

    Recursive Binary Search is done

--- Binary Search/Binary Search/Main.cpp
+++ Binary Search/Binary Search/Main.cpp
@@ -4,18 +4,36 @@ using namespace std;
 
 int recursiveBinarySearch(int arr[], int low, int high, int x)
 {
-
+	if (low > high)
+		return -1;
+	else
+	{
+		int mid = (high + low) / 2;
+		if (x == arr[mid])
+			return mid;
+		if (x < arr[mid])
+			return recursiveBinarySearch(arr, low, mid - 1, x);
+		return recursiveBinarySearch(arr, mid + 1, high, x);
+	}
 }
 
 int iterativeBinarySearch(int arr[], int low, int high, int x)
 {
 
-
+	return 0;
 }
 
 int main()
 {
 
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int high = sizeof(arr) / sizeof(arr[0]);
+	int low = 0;
+
+	int ReResult = recursiveBinarySearch(arr, low, high - 1, x);
+
+	cout << ReResult << endl;
 
 	return 0;
 }
